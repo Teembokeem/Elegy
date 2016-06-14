454dc5070ff8d3dc48db4de0cb41df5177a1f707
myunghoon kim <myungtkim@gmail.com>
2016-06-14 15:29:53 -0700

    adds config module to attach configs

---
+++ www/core/configs/log.config.js
@@ -0,0 +1,21 @@
+angular.module('Configs', [])
+
+.config(['$provide', function ($provide) {
+    $provide.decorator('$log', ['$delegate', function ($delegate) {
+        // Keep track of the original debug method, we'll need it later.
+        var origDebug = $delegate.debug;
+        /*
+         * Intercept the call to $log.debug() so we can add on 
+         * our enhancement. We're going to add on a date and 
+         * time stamp to the message that will be logged.
+         */
+        $delegate.controller = function(arg) {
+            var args = [].slice.call(arguments);
+           console.log("> " + arg + " controller activated. <")
+            
+            // Send on our enhanced message to the original debug method.
+         
+        }
+        return $delegate;
+    }]);
+}])
