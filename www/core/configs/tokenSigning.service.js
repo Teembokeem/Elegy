f408666e7e49fb90cf7dde3f3382da6e895b5a8f
myunghoon kim <myungtkim@gmail.com>
2016-06-15 18:26:07 -0700

    adds token singing interceptor

---
+++ www/core/configs/tokenSigning.service.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('Configs')
+    .factory('tokenSigningService', tokenSigningService);
+  
+  tokenSigningService.$inject = ['$log', 'tokenService'];
+
+  function tokenSigningService($log, tokenService) {
+    return {
+      request: signWithToken
+    };
+
+    function signWithToken(request) {
+      if (tokenService.retrieve()) {
+        $log.debug('Token exists: signing request.');
+        request.headers['Authorization'] = `Bearer ${tokenService.retrieve()}`;
+      }
+
+      return request;
+    }
+  }
+})();
