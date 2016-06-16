919d850b926a00514bcfb99ddcc08465533d732b
myunghoon kim <myungtkim@gmail.com>
2016-06-15 18:26:39 -0700

    adds http interceptor js delegating the two inteceptors for settings headers and inserting token into request

---
+++ www/core/configs/httpInterceptor.config.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('Configs')
+    .config(httpInterceptor)
+  
+  httpInterceptor.$inject = ['$httpProvider'];
+
+  function httpInterceptor($httpProvider) {
+    $httpProvider.interceptors.push('jsonHeadersService');
+    $httpProvider.interceptors.push('tokenSigningService');
+  }
+})();
