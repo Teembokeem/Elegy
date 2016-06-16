16afb965cb9802d107102930543211c00026464e
myunghoon kim <myungtkim@gmail.com>
2016-06-15 18:32:59 -0700

    adds auth service login. TODO: DEBUG TOMORROW

--- www/views/main/login/login.controller.js
+++ www/views/main/login/login.controller.js
@@ -6,9 +6,9 @@
     .module('Controllers')
     .controller('Login.controller', LoginController);
 
-  LoginController.$inject = ['$log', 'authService'];
+  LoginController.$inject = ['$log', 'authService', '$state'];
   
-  function LoginController($log, authService) {
+  function LoginController($log, authService, $state) {
     // INSTANTIATIONS
     $log.controller('Login')
     var vm = this;
@@ -22,7 +22,13 @@
     // BOUND FUNCTIONS
     vm.authenticate = function() {
       $log.info("Sending Credentials, ", vm.credentials);
-
+      authService.logIn(vm.credentials)
+      .then(function(decodedToken) {
+        $log.info("success", decodedToken);
+        $state.go('app.overview');
+      }, function(err) {
+        $log.info(err);
+      })
     }
     
     // HELPERS
