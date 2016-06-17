c55309d236793883d8b5fdf3ea29bb993d252dbe
myunghoon kim <myungtkim@gmail.com>
2016-06-17 11:09:24 -0700

    adds object structure for data payload, and adds bound function for submitting form to user serice http call

--- www/views/main/departed_signup/departed_signup.controller.js
+++ www/views/main/departed_signup/departed_signup.controller.js
@@ -6,16 +6,30 @@
     .module('Controllers')
     .controller('DepartedSignup.controller', DepartedSignupController);
   
-  DepartedSignupController.$inject = ['$log', '$state'];
+  DepartedSignupController.$inject = ['$log', 'userService'];
 
-  function DepartedSignupController($log, $state) {
+  function DepartedSignupController($log, userService) {
     // INSTANTIATIONS
     $log.controller('Departed Signup');
     var vm = this;
-    $log.debug("what", $state.$current)
+
     // LOCAL VARS
+    vm.newDeparted = {
+      first: '',
+      last: '',
+      dob: '',
+      dod: ''
+    }
 
     // BOUND FUNCTIONS
+    vm.submitDepartedForm = function() {
+      $log.info("Sending Departed Form, ", vm.newDeparted);
+      userService
+        .setupEvent(vm.newDeparted)
+        .then(function(res) {
+          $log.info('Successfully created assets: ', res.data);
+        })
+    }
 
     // HELPERS
   }
