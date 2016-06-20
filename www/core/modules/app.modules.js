2616b4a175ba61427ef52ed79b86be0ad40385d2
myunghoon kim <myungtkim@gmail.com>
2016-06-20 09:58:32 -0700

    finds fix for authorize routes on app module, evt to event.

--- www/core/modules/app.modules.js
+++ www/core/modules/app.modules.js
@@ -23,21 +23,21 @@
     .run(authorizeRoutes);
 
     
-    authorizeRoutes.$inject = ['$rootScope', '$log', 'authService', '$location'];
-    function authorizeRoutes($rootScope, $log, authService, $location) {
+    authorizeRoutes.$inject = ['$rootScope', '$log', 'authService', '$location', '$state'];
+    function authorizeRoutes($rootScope, $log, authService, $location, $state) {
       $rootScope.$on('$stateChangeStart', function(evt, toState, toParams, fromState, fromParams) {
         $log.debug('State Change Detected.', toParams);
 
         if (toState.url === '/guest-signup' && toParams.invCode === undefined) {
           $log.debug(`Attempted to go to ${toState.url} but could not find invite code parameters.`);
-          event.preventDefault();
-          $location.path('/app/referral');
+          evt.preventDefault();
+          $state.go('^.referral');
         }
         
         if (toState.authorized && !authService.isLoggedIn()) {
           $log.debug(`Attempted to go to ${toState.url} but no user credentials found.`);
-          event.preventDefault();
-          $location.path('/app/login');
+          evt.preventDefault();
+          $state.go('^.login');
         } else {
           $log.debug(`Traveling to ${toState.url} from ${fromState.url === "^" ? "reload" : fromState.url}. ${toState.controllerId} Controller to be loaded.`);
        }      
