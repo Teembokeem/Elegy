e06cee5dbff9c8080fbcba1240ce7b5895012f3c
myunghoon kim <myungtkim@gmail.com>
2016-06-21 16:15:10 -0700

    adds boiler local var to home controller

--- www/views/main/home/home.controller.js
+++ www/views/main/home/home.controller.js
@@ -1,19 +1,21 @@
 (function() {
   'use strict';
-  // TODO:
+  // TODO: 
+    // 1. set events in arrays based on whether event is past, or upcoming. needs at least date properties on event model..???
 
   angular
     .module('Controllers')
     .controller('Home.controller', HomeController);
   
-  HomeController.$inject = ['urlFactory', '$log'];
+  HomeController.$inject = ['urlFactory', '$log', 'authService'];
 
-  function HomeController(urlFactory, $log) {
+  function HomeController(urlFactory, $log, authService) {
     // INSTANTIATIONS
     $log.controller('Home');
     var vm = this;
 
     // LOCAL VARS
+    vm.assets = authService.currentUser();
 
     // BOUND FUNCTIONS
 
