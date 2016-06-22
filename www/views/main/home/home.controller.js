dbb1397db4b0caac2d92055aba52a738bbe26541
myunghoon kim <myungtkim@gmail.com>
2016-06-21 17:20:53 -0700

    minor setups

--- www/views/main/home/home.controller.js
+++ www/views/main/home/home.controller.js
@@ -1,22 +1,21 @@
 (function() {
   'use strict';
   // TODO: 
-    // 1. set events in arrays based on whether event is past, or upcoming. needs at least date properties on event model..???
+    // 1. set events in arrays based on whether event is past, or upcoming. needs at least date properties on event model..??? 
 
   angular
     .module('Controllers')
     .controller('Home.controller', HomeController);
   
-  HomeController.$inject = ['urlFactory', '$log', 'authService'];
+  HomeController.$inject = ['urlFactory', '$log', 'authService', 'dataService'];
 
-  function HomeController(urlFactory, $log, authService) {
+  function HomeController(urlFactory, $log, authService, dataService) {
     // INSTANTIATIONS
     $log.controller('Home');
     var vm = this;
 
     // LOCAL VARS
-    vm.assets = authService.currentUser();
-
+    vm.user = authService.currentUser();
     // BOUND FUNCTIONS
 
     // HELPERS
