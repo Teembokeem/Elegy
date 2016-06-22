524e7e3edbf459cd66711cc11db4f48d3cf40e6d
myunghoon kim <myungtkim@gmail.com>
2016-06-21 17:12:15 -0700

    adds setup for data service datatype delegator. prepares work for creating services for each data type.

--- www/core/factories/data.service.js
+++ www/core/factories/data.service.js
@@ -7,25 +7,41 @@
 
     var data = {}
 
-  dataService.$inject = ['$log'];
+  dataService.$inject = ['$log', 'eventService'];
 
-  function dataService($log) {
+  function dataService($log, eventService) {
     console.log('Data service loaded.');
 
     var service = {
       getData: getData,
       setData: setData
     };
-    
-    return service;
 
-    function getData(key) {
-      return JSON.parse(localStorage.getItem(key));
-    }
+    // EXPORTED FUNCTIONS
+    function getData(keys, params) {
+      var retrievalArray = [];
+      for (num in keys) {
+        retrievalArray.push(localStorage.getItem(keys[num]))
+      }
+      return applyOptions(retrievalArray, options[params])
+    };
 
     function setData(key, value) {
       localStorage.setItem(key, JSON.stringify(value));
     }
 
+    // HELPERS
+      // getData STACK
+      var options = {
+        event: eventService
+      }
+
+      function applyOptions(events) {
+        JSON.parse(events).forEach(function(event) {
+
+        })
+      }
+
+    return service;
   }
 })();
