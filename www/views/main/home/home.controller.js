aefba8fe5d19e19a885e540abe9a8dbe8fd329c4
myunghoon kim <myungtkim@gmail.com>
2016-06-22 15:04:33 -0700

    utilizes data binds on the resolve data in home state

--- www/views/main/home/home.controller.js
+++ www/views/main/home/home.controller.js
@@ -7,18 +7,22 @@
     .module('Controllers')
     .controller('Home.controller', HomeController);
   
-  HomeController.$inject = ['urlFactory', '$log', 'authService', 'dataService'];
+  HomeController.$inject = ['urlFactory', '$log', 'authService', 'dataService', 'events'];
 
-  function HomeController(urlFactory, $log, authService, dataService) {
+  function HomeController(urlFactory, $log, authService, dataService, events) {
     // INSTANTIATIONS
     $log.instantiate('Home', 'controller');
     var vm = this;
 
     // LOCAL VARS
     vm.user = authService.currentUser();
+    vm.assets = events[0].concat(events[1]);
+    $log.info(vm.assets)
+    
     // BOUND FUNCTIONS
-
+    
     // HELPERS
+
   }
 
 })();
