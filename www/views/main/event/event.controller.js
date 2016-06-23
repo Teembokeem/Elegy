08a2bd398b716a275820201f75640c173276f503
myunghoon kim <myungtkim@gmail.com>
2016-06-23 16:48:13 -0700

    adds test object, and method on swiping left to to the event controller

--- www/views/main/event/event.controller.js
+++ www/views/main/event/event.controller.js
@@ -12,13 +12,61 @@
     // INSTANTIATIONS
     $log.instantiate('Event', 'controller');
     var vm = this;
+    var setter = 0;
+
 
     // LOCAL VARS
+    vm.testObj = [
+      {
+        title: "Find A Venue",
+        parts: [
+          {
+            title: "Location",
+            description: "Lorem ipsum"
+          },
+          {
+            title: 'Price',
+            description: "Hallo"
+          }
+        ]
+      },
+      {
+        title: "Burial Preparations",
+        parts: [
+          {
+            title: "Casket",
+            description: "Lorem ipsum"
+          },
+          {
+            title: 'Flowers',
+            description: "Hallo"
+          },
+          {
+            title: 'Mural',
+            description: "artsy stuff"
+          }
+        ]
+      },
+      {
+        title: "Invite People",
+        parts: [
+          {
+            title: "Confirm Schedule and Program",
+            description: "Lorem ipsum"
+          },
+          {
+            title: 'Enter emails',
+            description: "Hallo"
+          }
+        ]
+      }
+    ]
+        vm.val = vm.testObj[setter];
 
     // BOUND FUNCTIONS
-    vm.okay = function() {
-      $log.info(dataService.getData(['event'],['event']))  
-  }
+    vm.slideStep = function() {
+      vm.val = vm.testObj[(setter ++) % 3]
+    }
     // HELPERS
     
   }
