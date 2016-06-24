d77d7f341396c97dc680044430749481567d42b1
myunghoon kim <myungtkim@gmail.com>
2016-06-24 14:06:14 -0700

    create vendor signup view controller module

--- www/views/main/vendor_signup/vendor_signup.controller.js
+++ www/views/main/vendor_signup/vendor_signup.controller.js
@@ -7,17 +7,37 @@
     .module('Controllers')
     .controller('VendorSignup.controller', VendorSignController);
   
-  VendorSignController.$inject = ['$log'];
+  VendorSignController.$inject = ['$log', 'userService', '$state'];
 
-  function VendorSignController($log) {
+  function VendorSignController($log, userService, $state) {
     // INSTANTIATIONS
     $log.instantiate('Vendor Signup', 'Controller')
     var vm = this;
 
 
     // LOCAL VARS
+    vm.newVendor = {
+      businessName: '',
+      address: '',
+      phone: {
+        personal: '',
+        business: ''
+      }
+    };
 
     // BOUND FUNCTIONS
+    vm.submitVendorForm = function() {
+      $log.info("Sending Form, ", vm.newVendor);
+      userService
+        .setupVendor(vm.newVendor)
+        .then(function(res) {
+          console.log("res?", res)
+          $state.go('app.vendor-tab.vendor-home');
+        })
+        .catch(function(err){
+          console.log("ohnoes,", err)
+        })
+    };
 
     // HELPERS
   }
