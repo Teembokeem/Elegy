7828bcce7fd9aef91bee8a1c71fcc35d9ae47006
myunghoon kim <myungtkim@gmail.com>
2016-06-24 17:30:00 -0700

    adds referral view controller module

--- www/views/main/referral/referral.controller.js
+++ www/views/main/referral/referral.controller.js
@@ -6,16 +6,41 @@
     .module('Controllers')
     .controller('Referral.controller', ReferralController);
   
-  ReferralController.$inject = ['$log'];
+  ReferralController.$inject = ['$log', '$ionicLoading', 'userService'];
 
-  function ReferralController($log) {
+  function ReferralController($log, $ionicLoading, userService) {
     // INSTANTIATIONS
     $log.instantiate("Referral", 'controller');
     var vm = this;
+    vm.newGuest = {
+      code: '',
+      email: ''
+    }
 
     // LOCAL VARS
 
     // BOUND FUNCTIONS
+
+    vm.validateCode = function() {
+      if (vm.newGuest.code.length === 6) {
+        $ionicLoading.show({
+          templateUrl: 'views/templates/loading.html'
+        }).then(function() {
+          userService
+            .setupGuest(vm.newGuest)
+            .then(function(done) {
+              $log.info("user Service setupGuest done.", done);
+              setTimeout(function() {
+                $ionicLoading.hide()
+                $ionicLoading.show({
+                  templateUrl: 'views/templates/done.html'
+                })
+              }, 3000)
+            })
+        })
+
+      }
+    }
 
     // HELPERS
   }
