2b35091178e2306244a20d4914110f5d9cc59b03
myunghoon kim <myungtkim@gmail.com>
2016-06-28 16:59:58 -0700

    shrinks authenticate function into one instead of querying different functions for vendor and user, uses backend logic to delegate routes afterwards.

--- www/views/main/login/login.controller.js
+++ www/views/main/login/login.controller.js
@@ -6,9 +6,9 @@
     .module('Controllers')
     .controller('Login.controller', LoginController);
 
-  LoginController.$inject = ['$log', 'authService', '$state', 'userService'];
+  LoginController.$inject = ['$log', 'authService', '$state', 'userService', '$ionicPopup'];
   
-  function LoginController($log, authService, $state, userService) {
+  function LoginController($log, authService, $state, userService, $ionicPopup) {
     // INSTANTIATIONS
     $log.instantiate('Login', 'controller')
     var vm = this;
@@ -21,25 +21,69 @@
     
     // BOUND FUNCTIONS
     vm.authenticate = function() {
-      $log.info("Sending Credentials, ", vm.credentials);
+      $log.info("Sending vendor credentials. ", vm.credentials);
+      authService.queryVendor(vm.credentials)
+        .then(function(res) {
+          $log.info("we back in here", res);
+          if (res.data.query) {
+            authService
+              .logIn(vm.credentials)
+              .then(function(decodedToken) {
+                $log.info("Credentials approved, ", decodedToken);
+                $state.go('app.vendor-tab.vendor-home')
+              })
+          } else {
+            // $ionicPopup.confirm({
+            //   templateUrl: 'views/templates/login_vendor_query.html',
+            //   buttons: [{
+            //     text: 'OK',
+            //     type: 'button-dark',
+            //     onTap: function(e) {
+            //       // Returning a value will cause the promise to resolve with the given value.
+            //       return;
+            //     }
+            //   }]
+            // })
+            // .then(function(res) {
+            //   vm.vendor = !vm.vendor
+            // })
+            authService
+              .logIn(vm.credentials)
+              .then(function(decodedToken) {
+                $log.info("Credentials approved, ", decodedToken);
+                
+                return userService.grabEventPackage(decodedToken._id)
+              })
+              .then(function(events) {
+                $log.info("event package?!?!?!!?!", events)
+              
+                $state.go('app.departed-tab.home');
+              })
+          }
+        })
+        .catch(function(err) {
+          $log.info("error in authenticate vendor method", err)
+        })
+    };
+
+    // vm.authenticate = function() {
+    //   $log.info("Sending Credentials, ", vm.credentials);
       
-      authService.logIn(vm.credentials)
-      .then(function(decodedToken) {
-        $log.info("Credentials approved, ", decodedToken);
-        return userService.grabEventPackage(decodedToken._id)
-      })
-      .then(function(events) {
-        $log.info("event package?!?!?!!?!", events)
-        if (vm.vendor) {
-          $state.go('app.vendor-tab.vendor-home');
-        } else {
-          $state.go('app.departed-tab.home');
-        }
-      })
-      .catch(function(err) {
-        $log.info("errrrrr", err)
-      })
-    }
+    //   authService.logIn(vm.credentials)
+    //     .then(function(decodedToken) {
+    //       $log.info("Credentials approved, ", decodedToken);
+          
+    //       return userService.grabEventPackage(decodedToken._id)
+    //     })
+    //     .then(function(events) {
+    //       $log.info("event package?!?!?!!?!", events)
+        
+    //       $state.go('app.departed-tab.home');
+    //     })
+    //     .catch(function(err) {
+    //       $log.info("errrrrr", err)
+    //     })
+    // }
     
     // HELPERS
   }
