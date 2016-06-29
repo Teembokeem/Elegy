2d1d893b8dc55c6560414296a49a61ba110ca9b2
myunghoon kim <myungtkim@gmail.com>
2016-06-29 12:06:28 -0700

    adds styling for vendor settings view controller module, with boiler controller

---
+++ www/views/main/vendor_settings/vendor_settings.controller.js
@@ -0,0 +1,23 @@
+(function() {
+'use strict';
+
+  angular
+    .module('Controllers')
+    .controller('VendorSettings.controller', VendorSettingsController);
+
+  VendorSettingsController.$inject = ['$log'];
+  function VendorSettingsController($log) {
+    // INSTANTIATIONS
+    $log.instantiate('Vendor Settings', 'Controller');
+    var vm = this;
+    
+    // LOCAL VARS
+
+    // BOUND FUNCTIONS
+
+    // HELPERS
+    
+
+    
+  }
+})();
