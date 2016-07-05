f087ef63e5c6f563fe4d1095ed99a22268c427de
myunghoon kim <myungtkim@gmail.com>
2016-07-05 15:17:42 -0700

    adds bound function for traversing to and from marketplace from event modules

--- www/views/main/event/event.controller.js
+++ www/views/main/event/event.controller.js
@@ -6,40 +6,36 @@
     .module('Controllers')
     .controller('Event.controller', EventController);
   
-  EventController.$inject = ['$log', 'dataService', '$scope', '$ionicModal', 'EventDataTemplates', 'EventStaticInfo'];
+  EventController.$inject = ['$log', 'dataService', '$scope', '$ionicModal', 'EventDataTemplates', 'EventStaticInfo', '$state'];
 
-  function EventController($log, dataService, $scope, $ionicModal, EventDataTemplates, EventStaticInfo) {
+  function EventController($log, dataService, $scope, $ionicModal, EventDataTemplates, EventStaticInfo, $state) {
     // INSTANTIATIONS
     $log.instantiate('Event', 'controller');
     var vm = this;
     var setter = 0;
     vm.EventDataTemplates = EventDataTemplates;
-    $log.instantiate('Event Data Template', 'constants')
     vm.EventStaticInfo = EventStaticInfo;
-    $log.instantiate('Event Static Info', 'constants')
-    $log.info('stuff', EventStaticInfo)
 
+    $log.instantiate('Event Data Template', 'constants')
+    $log.instantiate('Event Static Info', 'constants')
     // LOCAL VARS
         vm.val = vm.EventStaticInfo[setter];
 
     // BOUND FUNCTIONS
     vm.slideStep = function(dir) {
-      
       vm.val = vm.testObj[(((dir === 'left') ? setter ++ : setter = setter + 5)) % vm.testObj.length]
       var el = document.getElementsByClassName('step-container')[0];
-  
-      // -> triggering reflow /* The actual magic */
-      // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
-            // el.offsetWidth = el.offsetWidth;
-      
-      // -> and re-adding the class
       el.classList.add("step-title-out");
       el.classList.remove("step-title-in");
       setTimeout(function() {
         el.classList.add("step-title-in");
         el.classList.remove("step-title-out");
       }, 1000)
-    }
+    };
+
+    vm.displayMarketplace = function(param) {
+      $state.go('app.departed-tab.marketplace', {category: param})
+    };
 
 
     // HELPERS
