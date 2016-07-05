0c8c05a186e23b7d48dc14b9904e6c9818320593
myunghoon kim <myungtkim@gmail.com>
2016-07-05 14:24:09 -0700

    adds static info for event controller module to use as front facing data.

---
+++ www/core/constants/event.staticInfo.constants.js
@@ -0,0 +1,113 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('Constants')
+    .constant('EventStaticInfo', 
+      
+      [
+      {
+        title: "Find A Venue",
+        parts: [
+          {
+            title: "Location",
+            description: "Lorem ipsum",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Price',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          }
+        ]
+      },
+      {
+        title: "Major Decisions",
+        parts: [
+          {
+            title: "State Laws",
+            description: "Lorem ipsum",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Licensing',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Meeting with the Funeral Director',
+            description: "artsy stuff",
+            test: "FUCK YOUUUUUUUUUUU"
+          }
+        ]
+      },
+      {
+        title: "Keepsake Setup",
+        parts: [
+          {
+            title: "Confirm Payment Accounts",
+            description: "Lorem ipsum",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Add A Main Photo',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: "Write A Memorandum",
+            description: "Lorem ipsum",
+            test: "FUCK YOUUUUUUUUUUU"
+          }
+        ]
+      },
+      {
+        title: "General Preparations",
+        parts: [
+          {
+            title: "Create A Schedule",
+            description: "Lorem ipsum",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Plan the Day Of Program',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          }
+        ]
+      },
+      {
+        title: "Purchasables",
+        parts: [
+          {
+            title: "Flowers, Wreaths",
+            description: "Lorem ipsum",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Program Assets',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          }
+        ]
+      },
+      {
+        title: "Invitations",
+        parts: [
+          {
+            title: "Review Profile & items",
+            description: "Lorem ipsum",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Invite Friends and Family',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          }
+        ]
+      },
+
+      
+    ])
+
+})();
