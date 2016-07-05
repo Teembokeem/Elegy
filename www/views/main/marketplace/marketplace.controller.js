47fd09f61a91d12891b2140ff91b9d7afd5f3853
myunghoon kim <myungtkim@gmail.com>
2016-07-05 15:17:12 -0700

    adds boiler marketplace view controller module

---
+++ www/views/main/marketplace/marketplace.controller.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+  // TODO:
+    // 1.
+
+  angular
+    .module('Controllers')
+    .controller('Marketplace.controller', MarketplaceController);
+
+  MarketplaceController.$inject = ['$log', '$stateParams'];
+
+  function MarketplaceController($log, $stateParams) {
+    // INSTANTIATIONS
+    $log.instantiate('Marketplace', 'controller');
+    var vm = this;
+    $log.info("Displaying marketplace for: ", $stateParams.category)
+
+    // LOCAL VARS
+
+    // BOUND FUNCTIONS
+
+    // HELPERS
+  }
+
+})();
