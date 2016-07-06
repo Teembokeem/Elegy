959559f70d4827739b45ee93ae3b946b3685f714
myunghoon kim <myungtkim@gmail.com>
2016-07-06 14:03:50 -0700

    applies data paradigm to marketplace controller

--- www/views/main/marketplace/marketplace.controller.js
+++ www/views/main/marketplace/marketplace.controller.js
@@ -7,19 +7,22 @@
     .module('Controllers')
     .controller('Marketplace.controller', MarketplaceController);
 
-  MarketplaceController.$inject = ['$log', '$stateParams'];
+  MarketplaceController.$inject = ['$log', '$stateParams', '$location', 'dataService', '$state', 'Marketplace'];
 
-  function MarketplaceController($log, $stateParams) {
+  function MarketplaceController($log, $stateParams, $location, dataService, $state,  Marketplace) {
     // INSTANTIATIONS
     $log.instantiate('Marketplace', 'controller');
     var vm = this;
     vm.params = $stateParams.category
-    $log.info("Displaying marketplace for: ", $stateParams.category)
-
+    $log.info("Displaying marketplace for: ", $stateParams.category, Marketplace)
+    vm.listings = Marketplace[0]
     // LOCAL VARS
 
     // BOUND FUNCTIONS
-
+    vm.displayListing = function(data) {
+      dataService.setData(['listing'], [data]);
+      $state.go('app.departed-tab.listing', {venueName: data.title})
+    }
     // HELPERS
   }
 
