662ff92d83bdc52bb8a5d78d8c0ae5abf647f52f
ajchan11 <ajchan11@gmail.com>
2016-07-06 11:49:26 -0700

    working on feed views

--- www/views/main/feed/feed.controller.js
+++ www/views/main/feed/feed.controller.js
@@ -13,8 +13,34 @@
     $log.instantiate('Feed', 'controller');
     var vm = this;
 
-    // LOCAL VARS
+    console.log("FUCK YOU")
 
+    // LOCAL VARS
+    vm.items = [ 
+      { name: "Alex",
+        comment: "Something",
+        likes: 50 ,
+        commentCount: 20  },
+      { name: "Time",
+        comment: "Something",
+        likes: 6 ,
+        commentCount: 21  },
+      { name: "Poop",
+      comment: "Something",
+      likes: 7 ,
+      commentCount: 22  },
+      { name: "Greg",
+      comment: "Something",
+      likes: 8 ,
+      commentCount: 23  },
+      { name: "Blarg",
+      comment: "Something",
+      likes: 9 ,
+      commentCount: 24  },
+      { name: "GAH",
+      comment: "Something",
+      likes: 5 ,
+      commentCount: 25  }]
     // BOUND FUNCTIONS
 
     // HELPERS
