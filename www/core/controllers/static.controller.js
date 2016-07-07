2e0db2323b54cc50d3fadf4ce31a0a69ee6d4fc5
myunghoon kim <myungtkim@gmail.com>
2016-07-07 14:46:32 -0700

    adds a temp logout button for debugging purposes.

--- www/core/controllers/static.controller.js
+++ www/core/controllers/static.controller.js
@@ -7,12 +7,19 @@
     .module('Controllers')
     .controller('Static.controller', StaticController)
   
-  StaticController.$inject = ['$log'];
-  function StaticController($log) {
+  StaticController.$inject = ['$log', '$state', '$ionicHistory', '$window'];
+  function StaticController($log, $state, $ionicHistory, $window) {
     // INSTANTIATIONS
     $log.instantiate("Static", 'controller');
     var vm = this;
   
+    vm.logOut = function() {
+      $state.go('app.login');
+      $ionicHistory.clearHistory()
+      $ionicHistory.clearCache()
+      $window.localStorage.clear()
+      console.log("clearing caches.")
+    }
   }
     
 })();
