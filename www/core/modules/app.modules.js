606e865f7b5a58d59a2e5f9cec8caa8eeda1c44e
myunghoon kim <myungtkim@gmail.com>
2016-07-06 17:15:49 -0700

    adds boiler for listing view with reviews. this is to be finished.

--- www/core/modules/app.modules.js
+++ www/core/modules/app.modules.js
@@ -27,21 +27,29 @@
     function authorizeRoutes($rootScope, $log, authService, $location, $state) {
       $rootScope.$on('$stateChangeStart', function(evt, toState, toParams, fromState, fromParams) {
         $log.debug('State Change Detected.', toParams);
-
+        // var cnt = 0
+        // if (!cnt) {
+        //   cnt++;
+        //   $state.go('app.intro');
+        // }
         if (toState.url === '/guest-signup' && toParams.invCode === undefined) {
           $log.debug(`Attempted to go to ${toState.url} but could not find invite code parameters.`);
           evt.preventDefault();
           $state.go('^.referral');
+          return
         }
         
         if (toState.authorized && !authService.isLoggedIn()) {
           $log.debug(`Attempted to go to ${toState.url} but no user credentials found.`);
           evt.preventDefault();
           $state.go('^.login');
+          return
         } else {
           $log.debug(`Traveling to ${toState.url} from ${fromState.url === "^" ? "reload" : fromState.url}. ${toState.controllerId} Controller to be loaded.`);
+          return
        }      
       })
+
     }
 
   
