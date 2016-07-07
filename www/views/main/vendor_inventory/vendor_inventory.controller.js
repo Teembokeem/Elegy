d9368413b829df9ea43988912b4ccce3484d5c17
myunghoon kim <myungtkim@gmail.com>
2016-07-07 15:01:27 -0700

    adds boiler bound functions for state transition to vendor product view controller module

--- www/views/main/vendor_inventory/vendor_inventory.controller.js
+++ www/views/main/vendor_inventory/vendor_inventory.controller.js
@@ -7,9 +7,9 @@
     .module('Controllers')
     .controller('VendorInventory.controller', VendorInventoryController);
   
-  VendorInventoryController.$inject = ['$log'];
+  VendorInventoryController.$inject = ['$log', '$state'];
 
-  function VendorInventoryController($log) {
+  function VendorInventoryController($log, $state) {
     // INSTANTIATIONS
     $log.instantiate('Vendor Inventory', 'Controller');
     var vm = this;
@@ -18,6 +18,9 @@
 
 
     // BOUND FUNCTIONS
+    vm.createProduct = function() {
+      $state.go('app.vendor-tab.vendor-product');
+    };
 
 
     // HELPERS
