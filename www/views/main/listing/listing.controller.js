f2aac581130668d505ae7c0086617d228695d041
myunghoon kim <myungtkim@gmail.com>
2016-07-06 17:16:00 -0700

    boiler listing controller for the view controller module

--- www/views/main/listing/listing.controller.js
+++ www/views/main/listing/listing.controller.js
@@ -7,13 +7,16 @@
     .module('Controllers')
     .controller('Listing.controller', ListingController)
   
-  ListingController.$inject = ['$log'];
+  ListingController.$inject = ['$log', '$stateParams', 'Listing'];
 
-  function ListingController($log) {
+  function ListingController($log, $stateParams, Listing) {
     // INSTANTIATIONS
     $log.instantiate('Listing', 'controller');
     var vm = this;
-    
+    vm.venue = $stateParams.venueName;
+    vm.data = Listing[0]
+    $log.info("listing", Listing[0])
+
     // LOCAL VARS
 
     // BOUND FUNCTIONS
