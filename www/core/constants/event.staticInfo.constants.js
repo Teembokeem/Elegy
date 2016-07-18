eab1cc4a288b43ad981578038e9a8720df75b173
ajchan11 <ajchan11@gmail.com>
2016-07-18 10:15:49 -0700

    alex did more stuff

--- www/core/constants/event.staticInfo.constants.js
+++ www/core/constants/event.staticInfo.constants.js
@@ -6,32 +6,61 @@
     .constant('EventStaticInfo', 
       [
       {
-        title: "Find A Venue",
+        title: "Interment",
         step: 1,
         parts: [
           {
-            title: "Location",
-            description: "Compare funeral homes near you",
-            test: "Browse through locally listed funeral homes.",
-            category: 'Venues'
-          },
-          {
-            title: "Additional Information",
+            title: "Type of Interment",
             description: "",
             test: "",
           },
           {
-            title: 'Funeral Director',
-            description: "Reach out to a funeral home director",
-            test: "The director holds a lot of power, so make sure you find one you like. He or she will help guide you through the process."
+            title: "CRAPPPPP"
           }
         ],
         description: "Step 1",
         test: "What to consider when choosing a funeral home and director. Make sure to speak with more than one funeral home and compare any associated services."
       },
       {
-        title: "Invitations",
+        title: "Find Funeral Home",
         step: 2,
+        parts: [
+          
+        ],
+        description: "Step 2",
+        test: "FUCK YOUUUUUUUUUUU"
+      },
+      {
+        title: "Service Options",
+        step: 3,
+        parts: [
+          {
+            title: 'Visitation',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Ceremony',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Burial',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          },
+          {
+            title: 'Reception',
+            description: "Hallo",
+            test: "FUCK YOUUUUUUUUUUU"
+          }
+        ],
+        description: "Step 2",
+        test: "FUCK YOUUUUUUUUUUU"
+      },
+      {
+        title: "Invitations",
+        step: 4,
         parts: [
           {
             title: 'Invite Friends and Family',
@@ -43,31 +72,8 @@
         test: "FUCK YOUUUUUUUUUUU"
       },
       {
-        title: "Major Decisions",
-        step: 3,
-        parts: [
-          {
-            title: "Interment Arrangements",
-            description: "Lorem ipsum",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: 'Funeral Services',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: 'Schedule Date & Time',
-            description: "artsy stuff",
-            test: "FUCK YOUUUUUUUUUUU"
-          }
-        ],
-        description: "Step 3",
-        test: "FUCK YOUUUUUUUUUUU"
-      },
-      {
         title: "Keepsake Setup",
-        step: 4,
+        step: 5,
         parts: [
           {
             title: "Confirm Payment Accounts",
@@ -75,25 +81,7 @@
             test: "FUCK YOUUUUUUUUUUU"
           },
           {
-            title: 'Add A Main Photo',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
             title: "Write A Memorandum",
-            description: "Lorem ipsum",
-            test: "FUCK YOUUUUUUUUUUU"
-          }
-        ],
-        description: "Step 4",
-        test: "FUCK YOUUUUUUUUUUU"
-      },
-      {
-        title: "General Preparations",
-        step: 5,
-        parts: [
-          {
-            title: "Create A Schedule",
             description: "Lorem ipsum",
             test: "FUCK YOUUUUUUUUUUU"
           },
@@ -101,15 +89,7 @@
             title: 'Plan the Day Of Program',
             description: "Hallo",
             test: "FUCK YOUUUUUUUUUUU"
-          }
-        ],
-        description: "Step 5",
-        test: "FUCK YOUUUUUUUUUUU"
-      },
-      {
-        title: "Purchasables",
-        step: 6,
-        parts: [
+          },
           {
             title: "Flowers, Wreaths",
             description: "Lorem ipsum",
@@ -121,11 +101,9 @@
             test: "FUCK YOUUUUUUUUUUU"
           }
         ],
-        description: "Step 5",
+        description: "Step 4",
         test: "FUCK YOUUUUUUUUUUU"
-      }
-
-      
+      }     
     ])
 
 })();
