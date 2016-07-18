915ce09e68379e35ca110df33a4c109c6374f89c
myunghoon kim <myungtkim@gmail.com>
2016-07-18 16:58:40 -0700

    adds data binding for new directives

--- www/views/main/listing/listing.controller.js
+++ www/views/main/listing/listing.controller.js
@@ -7,21 +7,23 @@
     .module('Controllers')
     .controller('Listing.controller', ListingController)
   
-  ListingController.$inject = ['$log', '$stateParams', 'Listing', 'eventService', '$state'];
+  ListingController.$inject = ['$log', '$stateParams', 'Listing', 'eventService', '$state', 'ProductDataTemplates'];
 
-  function ListingController($log, $stateParams, Listing, eventService, $state) {
+  function ListingController($log, $stateParams, Listing, eventService, $state, ProductDataTemplates) {
     // INSTANTIATIONS
     $log.instantiate('Listing', 'controller');
     var vm = this;
-    vm.venue = $stateParams.venueName;
-    vm.data = Listing[0]
-    $log.info("listing", Listing[0])
+    vm.listingType = $stateParams.listingName;
+    vm.listing = Listing[0]
+    $log.info("listing", Listing[0]);
+
+
 
     // LOCAL VARS
 
     // BOUND FUNCTIONS
     vm.selectVenue = function() {
-      eventService.updateEvent(vm.data)
+      eventService.updateEvent(vm.listing)
       $state.go('app.departed-tab.event');
     }
 
