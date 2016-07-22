4216164819456235200ff74906e4bdcf3e6491d1
myunghoon kim <myungtkim@gmail.com>
2016-07-22 10:29:44 -0700

    adds static data bindings

--- www/views/main/event/event.controller.js
+++ www/views/main/event/event.controller.js
@@ -6,74 +6,73 @@
     .module('Controllers')
     .controller('Event.controller', EventController);
   
-  EventController.$inject = ['$log', 'dataService', '$scope', '$ionicModal', 'EventDataTemplates', 'EventStaticInfo', '$state', 'marketplaceService', '$stateParams'];
+  EventController.$inject = ['$log', 'dataService', '$scope', '$ionicModal', '$ionicPopup', 'EventDataTemplates', 'EventStaticInfo', '$state', 'marketplaceService', '$stateParams', 'eventService'];
 
-  function EventController($log, dataService, $scope, $ionicModal, EventDataTemplates, EventStaticInfo, $state, marketplaceService, $stateParams) {
+  function EventController($log, dataService, $scope, $ionicModal, $ionicPopup, EventDataTemplates, EventStaticInfo, $state, marketplaceService, $stateParams, eventService) {
     // INSTANTIATIONS
     $log.instantiate('Event', 'controller');
     var vm = this;
     var setter = 0;
     vm.EventDataTemplates = EventDataTemplates;
     vm.EventStaticInfo = EventStaticInfo;
+    $scope.eventStaticInfo = EventStaticInfo;
+    vm.eventModel = dataService.retrieveData('event');
     vm.part = {}
+    vm.eventStep = dataService.retrieveData('eventStep');
+    vm.stepIndex = dataService.retrieveData('stepIndex');
+    vm.eventStep ? vm.eventItems = vm.eventStep['types'].filter(function(type) {
+      return type.type === vm.eventModel['details'][vm.eventStep.title.toLowerCase()]['__t'].toLowerCase()
+    })[0]['parts'] : null;
+    
+    // setCompatibility(vm.eventItems);
 
-    // TODO: ON EVENT GRAB, USE CHOSEN INTERMENT TYPE:
-    var testobj = {
-      interment: {
-        traditional: {
-          casket: 'stuff'
-        }
-      }
-    }
-    var stateparamsStepNumber = 0;
-    vm.stepNumber = stateparamsStepNumber
-    vm.chosenVal = Object.keys(testobj.interment)[0]
-    vm.steps = vm['EventStaticInfo'][0]['types'][vm.chosenVal]['parts']
-
-    $log.info('Event Data Template', vm['EventStaticInfo'][0]['types'][vm.chosenVal]['parts'])
-    $log.instantiate('Event Data Template', 'constants')
-    $log.instantiate('Event Static Info', 'constants')
-
+    // LOGS FOR DATA CONFIRMS
+    $log.info("your event:", vm.eventModel)
+    $log.info("did it work?", vm.eventItems)
+    $log.info("did it work?", vm.trackers)
+    
     // LOCAL VARS
     
 
     // BOUND FUNCTIONS
-    // vm.slideStep = function(dir) {
-    //   vm.val = vm.EventStaticInfo[(((dir === 'right') ? setter = (setter + 1) % vm.EventStaticInfo.length : setter = (Math.abs((setter + vm.EventStaticInfo.length - 1)) % vm.EventStaticInfo.length)))]
-      
-    //   var el = document.getElementsByClassName('step-container')[0];
-      
-    //   // vm.openModal(vm.val)
-      
-    //   el.classList.add("step-title-out");
-    //   el.classList.remove("step-title-in");
-    //   setTimeout(function() {
-    //     el.classList.add("step-title-in");
-    //     el.classList.remove("step-title-out");
-    //   }, 1000)
-    // };
 
     vm.displayMarketplace = function(param) {
       $log.info("Event Controller display Marketplace method")
       var listings = marketplaceService.grabMarketplaceListings(param);
       listings.then(function(listings) {
-        dataService.setData(['listings'], [listings.data.data])
+        dataService.setData(['listings', 'stepItem'], [listings.data.data, param.toLowerCase()])
         $state.go('app.departed-tab.marketplace', {category: param})
       })
     };
 
-    vm.moveTo = function( part ) {
-      vm.part = part
-      $state.go('app.departed-tab.event', { stage: part.step})
+    vm.moveTo = function( part, index ) {
+      dataService.setData(['eventStep', 'stepIndex'], [part, index])
+      $state.go('app.departed-tab.event', { step: part.title})
+      setStepParameters(part.title);
     }
 
-    $stateParams.stage ? vm.val = EventStaticInfo[$stateParams.stage - 1] : true
+    function setStepParameters(prop) {
+      if (vm.eventModel[prop]) {
+      } else {
 
-    console.log( vm.thisStuff, "FUCKKK")
+      }
+    }
 
+    vm.setupModelOptions = function(option) {
+      $log.instantiate("Event controller setupModelOptions", 'Method');
+      
+      return eventService
+              .setupModelOptions(dataService.retrieveData('event')._id, option, dataService.retrieveData('eventStep').title)
+              .then(function(res) {
+                $log.info("success", res)
+                dataService.setData(['event'], [res]);
+                vm.eventModel = dataService.retrieveData('event');
+                $log.info(vm.eventModel)
+              })
+  }
 
+    // HELPER
 
-    // HELPERS
     $ionicModal.fromTemplateUrl('views/templates/modal.html', {
       scope: $scope,
       animation: 'slide-in-right'
@@ -107,6 +106,9 @@
     $scope.$on('modal.removed', function() {
       // Execute action
     });
+
+
+    
   }
 
 })();
