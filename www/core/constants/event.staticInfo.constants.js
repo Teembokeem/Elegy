a67a049421b8cfb0bc480859909cd60fe799022f
myunghoon kim <myungtkim@gmail.com>
2016-07-22 10:27:57 -0700

    reorganizing for static info

--- www/core/constants/event.staticInfo.constants.js
+++ www/core/constants/event.staticInfo.constants.js
@@ -4,73 +4,118 @@
   angular
     .module('Constants')
     .constant('EventStaticInfo', 
-      [
-      {
-        title: "Interment",
-        step: 1,
-        types: {
-          traditional: {
-            parts: [
-              {
-                title: "Casket",
-                description: "Compare funeral homes near you",
-                test: "Browse through locally listed funeral homes.",
-                category: 'Casket'
-              },
-              {
-                title: "Burial Vault",
-                description: "Compare funeral homes near you",
-                test: "Browse through locally listed funeral homes.",
-                category: 'BurialVaults'
-              },
-              {
-                title: "Burial Marker",
-                description: "Compare funeral homes near you",
-                test: "Browse through locally listed funeral homes.",
-                category: 'Markers'
-              }
-            ],
-          }
-        },
-        description: "Step 1",
-        test: "What to consider when choosing a funeral home and director. Make sure to speak with more than one funeral home and compare any associated services."
+      
+      [{
+          title: "Interment",
+          step: 1,
+          description: "Step 1",
+          test: "What to consider when choosing a funeral home and director. Make sure to speak with more than one funeral home and compare any associated services.",
+          types: [
+            { 
+              type: 'traditional', 
+              parts: [
+                {
+                  title: "Casket",
+                  description: "Compare funeral homes near you",
+                  test: "Browse through locally listed funeral homes.",
+                  category: 'Casket',
+                  tracker: 'casket'
+                },
+                {
+                  title: "Burial Vault",
+                  description: "Compare funeral homes near you",
+                  test: "Browse through locally listed funeral homes.",
+                  category: 'BurialVault',
+                  tracker: 'burialVault'
+                },
+                {
+                  title: "Burial Marker",
+                  description: "Compare funeral homes near you",
+                  test: "Browse through locally listed funeral homes.",
+                  category: 'Marker',
+                  tracker: 'marker'
+                }
+              ]
+            },
+            { type: 'cremation',
+              parts: [
+                {
+                  title: "Urn",
+                  description: "Compare funeral homes near you",
+                  test: "Browse through locally listed funeral homes.",
+                  category: 'Urn',
+                  tracker: 'urn'
+                }
+              ],  
+            },
+            {type:'graveside',
+              parts: [
+                {
+                  title: "Urn",
+                  description: "Compare funeral homes near you",
+                  test: "Browse through locally listed funeral homes.",
+                  category: 'Urn',
+                  tracker: 'urn'
+                }
+              ]
+            }, 
+            {type: 'bodyDonation',
+              parts: [
+                {
+                  title: "Donation Location",
+                  description: "Compare funeral homes near you",
+                  test: "Browse through locally listed funeral homes.",
+                  category: 'BodyDonation',
+                  tracker: 'bodyDonation'
+                }
+              ], 
+            }
+          ]
       },
       {
         title: "Find Funeral Home",
         step: 2,
-        parts: [
-          
-        ],
         description: "Step 2",
-        test: "FUCK YOUUUUUUUUUUU"
+        test: "What to consider when choosing a funeral home and director. Make sure to speak with more than one funeral home and compare any associated services.",
+        types: [
+          {
+            parts: [
+              {
+                title: 'Visitation',
+                description: "Compare funeral homes near you",
+                test: "Browse through locally listed funeral homes.",
+                tracker: 'visitation'
+              },
+              {
+                title: 'Ceremony',
+                description: "Compare funeral homes near you",
+                test: "Browse through locally listed funeral homes.",
+                category: 'Churches',
+                tracker: 'church'
+              }
+            ]
+          }
+        ]
       },
       {
         title: "Service Options",
         step: 3,
-        parts: [
+        description: "Step 3",
+        test: "FUCK YOUUUUUUUUUUU",
+        types: [
           {
-            title: 'Visitation',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: 'Ceremony',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: 'Burial',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: 'Reception',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
+            title: "",
+            description: "Compare funeral homes near you",
+            test: "Browse through locally listed funeral homes.",
+            category: 'BodyDonation',
+            tracker: 'bodyDonation',
+            parts: [
+              {
+
+              }
+            ]
           }
         ],
-        description: "Step 2",
-        test: "FUCK YOUUUUUUUUUUU"
       },
       {
         title: "Invitations",
@@ -118,6 +163,6 @@
         description: "Step 4",
         test: "FUCK YOUUUUUUUUUUU"
       }     
-    ])
+      ])
 
 })();
