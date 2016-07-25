2f1490e0a49f947ab473a9dd439691a638e58744
myunghoon kim <myungtkim@gmail.com>
2016-07-25 13:15:32 -0700

    adds trnsaction service for api calls

--- www/core/constants/event.staticInfo.constants.js
+++ www/core/constants/event.staticInfo.constants.js
@@ -7,6 +7,7 @@
       
       [{
           title: "Interment",
+          eventKey: 'interment',
           step: 1,
           description: "Step 1",
           test: "What to consider when choosing a funeral home and director. Make sure to speak with more than one funeral home and compare any associated services.",
@@ -74,9 +75,30 @@
       },
       {
         title: "Find Funeral Home",
+        eventKey: 'funeralHome',
         step: 2,
         description: "Step 2",
         test: "What to consider when choosing a funeral home and director. Make sure to speak with more than one funeral home and compare any associated services.",
+        types: [
+          {
+            parts: [
+              {
+                title: 'Funeral Home',
+                description: "Compare funeral homes near you",
+                test: "Browse through locally listed funeral homes.",
+                category: 'FuneralHome',
+                tracker: 'funeralHome'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        title: "Service Options",
+        eventKey: 'options',
+        step: 3,
+        description: "Step 3",
+        test: "FUCK YOUUUUUUUUUUU",
         types: [
           {
             parts: [
@@ -92,76 +114,94 @@
                 test: "Browse through locally listed funeral homes.",
                 category: 'Churches',
                 tracker: 'church'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        title: "Service Options",
-        step: 3,
-        description: "Step 3",
-        test: "FUCK YOUUUUUUUUUUU",
-        types: [
-          {
-            title: "",
-            description: "Compare funeral homes near you",
-            test: "Browse through locally listed funeral homes.",
-            category: 'BodyDonation',
-            tracker: 'bodyDonation',
-            parts: [
+              },
               {
-
-              }
+                title: 'Burial',
+                description: "Compare funeral homes near you",
+                test: "Browse through locally listed funeral homes.",
+                tracker: 'burial'
+              },
+              {
+                title: 'Reception',
+                description: "Compare funeral homes near you",
+                test: "Browse through locally listed funeral homes.",
+                category: 'Venue',
+                tracker: 'venue'
+              },
             ]
           }
         ],
       },
       {
         title: "Invitations",
+        eventKey: 'inviteGuests',
         step: 4,
-        parts: [
-          {
-            title: 'Invite Friends and Family',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
-          }
-        ],
         description: "Step 2",
-        test: "FUCK YOUUUUUUUUUUU"
+        test: "FUCK YOUUUUUUUUUUU",
+        types: [
+          {
+            parts: [
+                {
+                title: 'Email entry',
+                description: "Compare funeral homes near you",
+                test: "Browse through locally listed funeral homes.",
+                tracker: 'attendees'
+              },
+              {
+                title: 'Contact Upload',
+                description: "Compare funeral homes near you",
+                test: "Browse through locally listed funeral homes.",
+                tracker: 'attendees'
+              },
+              {
+                title: 'Text Message',
+                description: "Compare funeral homes near you",
+                test: "Browse through locally listed funeral homes.",
+                tracker: 'attendees'
+              },
+            ]
+          }
+        ]
       },
       {
         title: "Keepsake Setup",
+        eventKey: 'keepSake',
         step: 5,
-        parts: [
-          {
-            title: "Confirm Payment Accounts",
-            description: "Lorem ipsum",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: "Write A Memorandum",
-            description: "Lorem ipsum",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: 'Plan the Day Of Program',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: "Flowers, Wreaths",
-            description: "Lorem ipsum",
-            test: "FUCK YOUUUUUUUUUUU"
-          },
-          {
-            title: 'Program Assets',
-            description: "Hallo",
-            test: "FUCK YOUUUUUUUUUUU"
-          }
-        ],
         description: "Step 4",
-        test: "FUCK YOUUUUUUUUUUU"
+        test: "FUCK YOUUUUUUUUUUU",
+        types: [
+          {
+            parts: [
+              {
+                title: "Confirm Payment Accounts",
+                description: "Lorem ipsum",
+                test: "FUCK YOUUUUUUUUUUU"
+              },
+              {
+                title: "Write A Memorandum",
+                description: "Lorem ipsum",
+                test: "FUCK YOUUUUUUUUUUU"
+              },
+              {
+                title: 'Plan the Day Of Program',
+                description: "Hallo",
+                test: "FUCK YOUUUUUUUUUUU"
+              },
+              {
+                title: "Flowers, Wreaths",
+                description: "Lorem ipsum",
+                test: "FUCK YOUUUUUUUUUUU",
+                category: 'Flower',
+                tracker: 'flower'
+              },
+              {
+                title: 'Find Counselor',
+                description: "Hallo",
+                test: "FUCK YOUUUUUUUUUUU"
+              }
+            ]
+          }
+        ]
       }     
       ])
 
