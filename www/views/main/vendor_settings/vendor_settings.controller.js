d724cf5d1c444abcc1e5522fae6d96970c418450
myunghoon kim <myungtkim@gmail.com>
2016-07-27 16:40:12 -0700

    adds setting logic for venue vendors

--- www/views/main/vendor_settings/vendor_settings.controller.js
+++ www/views/main/vendor_settings/vendor_settings.controller.js
@@ -5,11 +5,12 @@
     .module('Controllers')
     .controller('VendorSettings.controller', VendorSettingsController);
 
-  VendorSettingsController.$inject = ['$log'];
-  function VendorSettingsController($log) {
+  VendorSettingsController.$inject = ['$log', 'dataService'];
+  function VendorSettingsController($log, dataService) {
     // INSTANTIATIONS
     $log.instantiate('Vendor Settings', 'Controller');
     var vm = this;
+    vm.activeListing;
     
     // LOCAL VARS
 
@@ -19,5 +20,11 @@
     
 
     
+      var vendorProduct = dataService.retrieveData('vendorProducts').filter(function(product) {
+        return product.__t === 'Venue'
+      })
+      if (vendorProduct) {
+        vm.activeListing = vendorProduct[0]['activeListing'];
+      }
   }
 })();
