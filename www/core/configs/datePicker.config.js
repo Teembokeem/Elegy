626f0e1f313e8b1aabc1439a95f362ddd2233de8
myunghoon kim <myungtkim@gmail.com>
2016-07-27 16:38:04 -0700

    adds datepicker for date picking

---
+++ www/core/configs/datePicker.config.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('Configs')
+    .config(function (ionicDatePickerProvider) {
+    var datePickerObj = {
+      inputDate: new Date(),
+      setLabel: 'Set',
+      todayLabel: 'Today',
+      closeLabel: 'Close',
+      mondayFirst: false,
+      weeksList: ["S", "M", "T", "W", "T", "F", "S"],
+      monthsList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+      templateType: 'popup',
+      from: new Date(),
+      to: new Date(2016, 12, 31),
+      showTodayButton: true,
+      dateFormat: 'dd MMMM yyyy',
+      closeOnSelect: false,
+      disableWeekdays: []
+    };
+    ionicDatePickerProvider.configDatePicker(datePickerObj);
+  })
+
+})();
