d86cdba46c9abca770fb5107f2880128ec6c23ff
myunghoon kim <myungtkim@gmail.com>
2016-07-27 16:38:48 -0700

    adds transaction service method for braint ree setup

--- www/core/factories/transaction.service.js
+++ www/core/factories/transaction.service.js
@@ -1,41 +1,43 @@
 (function() {
   'use strict'
 
+  console.log("transaction service")
   angular
     .module('Services')
     .factory('transactionService', transactionService);
 
-  transactionService.$inject = ['$log', '$http'];
+  transactionService.$inject = ['$log', '$http', 'urlFactory'];
 
-  function transactionService($log, $http) {
+  function transactionService($log, $http, urlFactory) {
     // INSTANTIATIONS
     $log.instantiate('Transaction', 'Service');
 
     var service = {
       initializeBrainTree: initializeBrainTree
     }
+    return service;
 
 
     function initializeBrainTree() {
-      $http({
+      $log.instantiate('Transaction Service Initialize BrainTree', 'method')
+      return $http({
         method: 'GET',
-        url: url + '/client_token'
+        url: urlFactory + '/client_token'
       })
       .then(function(response) {
-        $log.debug("we got it back:", response.data);
-        braintree.setup(
-          response.data,
-          "dropin",
-          { container: "payment-form",
-            paymentMethodNonceReceived: function(event, nonce) {
-              $log.debug("what", nonce, event)
-            }
-          }
-        )
+        $log.debug("success", response.data);
+        // braintree.setup(
+        //   response.data,
+        //   "dropin",
+        //   { container: "payment-form",
+        //     paymentMethodNonceReceived: function(event, nonce) {
+        //       $log.debug("what", nonce, event)
+        //     }
+        //   }
+        // )
       })
     }
 
-    return service;
   } 
 
 })();
