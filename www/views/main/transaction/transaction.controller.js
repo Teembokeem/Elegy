e8ffb5c1f3132ff44a6733ecc63dc58ff09f8b5d
myunghoon kim <myungtkim@gmail.com>
2016-08-01 14:20:51 -0700

    data bindings for transaction controller

--- www/views/main/transaction/transaction.controller.js
+++ www/views/main/transaction/transaction.controller.js
@@ -15,27 +15,34 @@
     var vm = this;
     var clientToken = brainTree;
     if (clientToken != null) {
-      $log.info("client Token secured.")
+      $log.info("Client Token secured.")
     }
 
     // LOCAL VARS
     vm.item = [dataService.retrieveData('listing')]
-    vm.vendor = dataService.retrieveData('vendor')
+    vm.vendor = dataService.retrieveData('vendor');
+    vm.itemPrice = parseFloat(vm.item[0].price);
+    vm.serviceFee = 3.00;
+    vm.checkout = false;
 
     // LOGS
-    $log.info("vm.item:", vm.item)
-    $log.info("vm.vendor:", vm.vendor)
+    $log.info("vm.item:", vm.item, parseFloat(3.00));
+    $log.info("vm.vendor:", vm.vendor);
 
     // BOUND FUNCTIONS
-    braintree.setup(
-          brainTree,
-          "dropin",
-          { container: "payment-form",
-            paymentMethodNonceReceived: function(event, nonce) {
-              $log.debug("what", nonce, event)
+    vm.checkOut = function() {
+      $log.instantiate('Transaction Controller CheckOut', 'Method');
+      vm.checkout = true;
+      braintree.setup(
+            brainTree,
+            "dropin",
+            { container: "payment-form",
+              paymentMethodNonceReceived: function(event, nonce) {
+                $log.debug("nonce received", nonce, event)
+              }
             }
-          }
-        )
+          )
+    }
 
     // HELPERS
 
