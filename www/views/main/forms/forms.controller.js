43982120f52a0bb0e6418ba046f4351187af9808
myunghoon kim <myungtkim@gmail.com>
2016-08-03 16:57:45 -0700

    adds data binding to use for update event method

--- www/views/main/forms/forms.controller.js
+++ www/views/main/forms/forms.controller.js
@@ -8,13 +8,17 @@
     .module('Controllers')
     .controller('Forms.controller', FormsController);
   
-  FormsController.$inject = ['$log', '$stateParams', 'dataService'];
+  FormsController.$inject = ['$log', '$stateParams', 'dataService', 'eventService', '$state'];
 
-  function FormsController($log, $stateParams, dataService) {
+  function FormsController($log, $stateParams, dataService, eventService, $state) {
     // INSTANTIATIONS
     $log.instantiate('Forms', 'Controller');
     var vm = this;
     vm.formType = $stateParams.tracker;
+    vm.formPart;
+    var eventStep = dataService.retrieveData('eventStep')
+    vm.step = dataService.retrieveData('eventStep')['eventKey']
+    vm.eventId = dataService.retrieveData('event')['_id']
     vm.newMember = null;
     vm.newOfficiant = null;
     vm.editingOfficiant = false;
@@ -32,18 +36,21 @@
       switch(formPart) {
         case 'members':
         $log.info("doing it 1")
+          vm.formPart = formPart;
           vm.program[formPart].push(data);
           vm.newMember = null;
           $log.info(vm.program);          
           break;
         case 'pallbearers':
         $log.info("doing it 2")
+          vm.formPart = formPart;
           vm.program[formPart].push(data);
           vm.newBearer = null;    
           $log.info(vm.program);      
           break;
         case 'officiant':  
         $log.info("doing it 3")
+          vm.formPart = formPart;
           vm.editingOfficiant = true;
           vm.program.officiant = data;
           vm.newOfficiant = null;
@@ -51,6 +58,7 @@
           break;
         case 'eulogizer':  
         $log.info("doing it 4")
+          vm.formPart = formPart;
           vm.editingEulogizer = true;
           vm.program.eulogizer = data;
           vm.newEulogizer = null;
@@ -58,11 +66,13 @@
           break;
         case 'songs':
         $log.info("doing it 5")
+          vm.formPart = formPart;
           vm.program[formPart].push(data);
           vm.newSong = null;          
           break;
         case 'readings':
         $log.info("doing it 6")
+          vm.formPart = formPart;
           vm.program[formPart].push(data);
           vm.newReading = null;          
           break;
@@ -71,6 +81,23 @@
 
     vm.updateProgram = function() {
       $log.info("hi")
+      eventService
+        .updateEvent(vm.program, vm.eventId, vm.step, vm.formType, vm.formPart)
+        .then(function(done) {
+          $log.info("Success", done);
+          eventService
+            .retrieveEvent(vm.eventId)
+            .then(function(done) {
+              dataService.setData(['event'], [done])
+              $state.go('app.departed-tab.event', {step: eventStep.tracker})
+            })
+            .catch(function(err) {
+              $log.info("error", err)
+            })
+        })
+        .catch(function(err) {
+          $log.info("error", err)
+        })
     }
 
     // HELPERS
