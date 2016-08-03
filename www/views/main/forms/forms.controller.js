a58756c882437cd9204e0d68d7463804124bb548
myunghoon kim <myungtkim@gmail.com>
2016-08-03 15:06:58 -0700

    adds switch cases for ng-switches and process actions by those items

--- www/views/main/forms/forms.controller.js
+++ www/views/main/forms/forms.controller.js
@@ -8,17 +8,45 @@
     .module('Controllers')
     .controller('Forms.controller', FormsController);
   
-  FormsController.$inject = ['$log', '$stateParams'];
+  FormsController.$inject = ['$log', '$stateParams', 'dataService'];
 
-  function FormsController($log, $stateParams) {
+  function FormsController($log, $stateParams, dataService) {
     // INSTANTIATIONS
     $log.instantiate('Forms', 'Controller');
     var vm = this;
-    vm.formType = $stateParams.tracker
+    vm.formType = $stateParams.tracker;
+    vm.newMember = null;
+    vm.newOfficiant = null;
+    vm.editingOfficiant = false;
 
     // LOCAL VARS
-
+    switch(vm.formType) {
+      case 'program':
+        $log.info("program case");
+        vm.program = dataService.retrieveData('event')['details']['keepsake']['program']
+        break;
+    }
     // BOUND FUNCTIONS
+    vm.processAction = function(formPart, data) {
+      switch(formPart) {
+        case 'members':
+        $log.info("doing it 1")
+          vm.program[formPart].push(data);
+          vm.newMember = null;          
+          break;
+        case 'bearers':
+        $log.info("doing it 2")
+          vm.program[formPart].push(data);
+          vm.newBearer = null;          
+          break;
+        case 'officiant':  
+        $log.info("doing it 3")
+          vm.editingOfficiant = true;
+          vm.program.officiant = data;
+          vm.newOfficiant = null;
+          $log.info(vm.program)
+      }
+    }
 
     // HELPERS
   }
