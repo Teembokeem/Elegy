5a3c8271bd99db2d55d080f4c9ccbec1477ab90d
myunghoon kim <myungtkim@gmail.com>
2016-08-03 16:12:15 -0700

    adds switch case for the rest of the program

--- www/views/main/forms/forms.controller.js
+++ www/views/main/forms/forms.controller.js
@@ -18,6 +18,7 @@
     vm.newMember = null;
     vm.newOfficiant = null;
     vm.editingOfficiant = false;
+    vm.editingEulogizer = false;
 
     // LOCAL VARS
     switch(vm.formType) {
@@ -32,20 +33,44 @@
         case 'members':
         $log.info("doing it 1")
           vm.program[formPart].push(data);
-          vm.newMember = null;          
+          vm.newMember = null;
+          $log.info(vm.program);          
           break;
-        case 'bearers':
+        case 'pallbearers':
         $log.info("doing it 2")
           vm.program[formPart].push(data);
-          vm.newBearer = null;          
+          vm.newBearer = null;    
+          $log.info(vm.program);      
           break;
         case 'officiant':  
         $log.info("doing it 3")
           vm.editingOfficiant = true;
           vm.program.officiant = data;
           vm.newOfficiant = null;
+          $log.info(vm.program);
+          break;
+        case 'eulogizer':  
+        $log.info("doing it 4")
+          vm.editingEulogizer = true;
+          vm.program.eulogizer = data;
+          vm.newEulogizer = null;
           $log.info(vm.program)
+          break;
+        case 'songs':
+        $log.info("doing it 5")
+          vm.program[formPart].push(data);
+          vm.newSong = null;          
+          break;
+        case 'readings':
+        $log.info("doing it 6")
+          vm.program[formPart].push(data);
+          vm.newReading = null;          
+          break;
       }
+    }
+
+    vm.updateProgram = function() {
+      $log.info("hi")
     }
 
     // HELPERS
