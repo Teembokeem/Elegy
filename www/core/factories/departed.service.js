574c19102fb6b42e00d438747a012b7dc11e4444
myunghoon kim <myungtkim@gmail.com>
2016-08-04 11:57:23 -0700

    adds departed service

---
+++ www/core/factories/departed.service.js
@@ -0,0 +1,36 @@
+(function() {
+'use strict';
+
+  angular
+    .module('Services')
+    .factory('departedService', departedService);
+
+  departedService.$inject = ['$log', '$http', 'urlFactory'];
+  function departedService($log, $http, urlFactory) {
+    var service = {
+      updateEulogy: updateEulogy
+    };
+    
+    return service;
+
+    ////////////////
+    function updateEulogy(id, eulogy) { 
+      $log.instantiate("Departed Service Update Eulogy", 'method');
+      $log.info("id", id, eulogy)
+      return $http({
+          method: 'PUT',
+          url: urlFactory + '/departed/' + id,
+          data: {
+            eulogy: eulogy
+          }
+        })
+        .then(function(done) {
+          return done.data.data;
+        })
+        .catch(function(err) {
+          return err;
+        })
+      }
+
+  }
+})();
