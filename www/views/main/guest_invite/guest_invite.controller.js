07cef094c78aa60ad26f708a7ce2638398f6a7c3
myunghoon kim <myungtkim@gmail.com>
2016-08-04 16:59:02 -0700

    adds logic for handling outbound user servicecreate guests and sending invitations method.

--- www/views/main/guest_invite/guest_invite.controller.js
+++ www/views/main/guest_invite/guest_invite.controller.js
@@ -6,13 +6,17 @@
     .module('Controllers')
     .controller('GuestInvite.controller', GuestInviteController);
   
-  GuestInviteController.$inject = ['$log', 'dataService', '$http', 'urlFactory', '$cordovaContacts', '$scope', '$ionicPlatform'];
+  GuestInviteController.$inject = ['$log', 'dataService', '$http', 'urlFactory', '$cordovaContacts', '$scope', '$ionicPlatform', 'userService', 'eventService'];
 
-  function GuestInviteController($log, dataService, $http, urlFactory, $cordovaContacts, $scope, $ionicPlatform) {
+  function GuestInviteController($log, dataService, $http, urlFactory, $cordovaContacts, $scope, $ionicPlatform, userService, eventService) {
     // INSTANTIATIONS
     $log.instantiate('Feed', 'controller');
     var vm = this;
+    var departed = dataService.retrieveData('departed');
+    var event = dataService.retrieveData('event');
+    var step = dataService.retrieveData('eventStep');
     vm.guestList = []
+    
 
 
     vm.addPerson = function( email ) {
@@ -30,13 +34,23 @@
     vm.sendInvitations = function() {
       $log.instantiate("Guest Invite Controller Send Invitations", 'method')
       console.log("Final Guest List", vm.guestList)
-      eventService
-        .sendInvitations
+      userService
+        .createGuests(vm.guestList, departed.inviteHash)
         .then(function(done) {
-          $log.info("success: ", done)
-        })
-        .catch(function(err) {
-          $log.info("ehoh error: ", err)
+          var newArr = [];
+          done.forEach(function(user) {
+              //  TODO: SET THIS PART UP TO PARSE AND CREATE THE OBJECT AS SEEN IN EVENT MODEL BACKEND. 
+              // BUTTTTTT THIS ALSO NEEDS TO CHECK FOR IF A USER WERE TO INVITE ADDITIONAL GUESTS AT A LATER TIME, MEANING THE ARRAY IN THE BACKEND EXISTS.....
+              // WHICH MEANS YOU SHOULD PROBABLY ADD IN A CHECK SOMEWHERE FOR GRABBING THAT ATTENDEES LIST, AND PUSHING IT TO THAT ARRAY. KBYE.
+          })
+          eventService
+            .updateEvent(done, event._id, step.eventKey, step.types[0]['parts'][0]['tracker'])
+            .then(function(done) {
+              $log.info("success: ", done)
+            })
+            .catch(function(err) {
+              $log.info("ehoh error: ", err)
+            })
         })
     }
 
