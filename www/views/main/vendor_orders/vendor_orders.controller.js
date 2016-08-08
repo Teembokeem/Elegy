37a8032b31c7b35435ed4c0555328330462a7930
myunghoon kim <myungtkim@gmail.com>
2016-08-08 16:31:52 -0700

    adds boiler for data bindings. prepares work for emitting statuses on item delivery types

--- www/views/main/vendor_orders/vendor_orders.controller.js
+++ www/views/main/vendor_orders/vendor_orders.controller.js
@@ -7,14 +7,15 @@
     .module('Controllers')
     .controller('VendorOrders.controller', VendorOrdersController);
 
-  VendorOrdersController.$inject = ['$log'];
-  function VendorOrdersController($log) {
+  VendorOrdersController.$inject = ['$log', 'dataService'];
+  function VendorOrdersController($log, dataService) {
     // INSTANTIATIONS
     $log.instantiate('Vendor Orders', 'Controller')
     var vm = this;
 
     // LOCAL VARS
     vm.passive = 'completedOrders';
+    vm.orders = dataService.retrieveData('vendorOrders');
 
     // BOUND FUNCTIONS
 
