2c36500c890afca3ea9158f5ffe53ebd41b93a6a
myunghoon kim <myungtkim@gmail.com>
2016-08-09 16:20:59 -0700

    adds logic for submitting forms

--- www/views/main/user_signup/user_signup.controller.js
+++ www/views/main/user_signup/user_signup.controller.js
@@ -26,23 +26,40 @@
     //  BOUND FUNCTIONS
     vm.submitUserForm = function() {
       $log.info("Sending Form, ", vm.newUser);
-      userService
-        .signup(vm.newUser)
-        .then(function(res) {
-            $log.info('Successfully created user, ' + res.data.data.email + ' ' +  res.data.id, res.data);
-            return authService.logIn(vm.newUser);
-        })
-        .then(function(decodedToken) {
-          $log.info('Logged In via Auth service login. ', decodedToken);
-          if (vm.vendor) {
-            $state.go('app.vendor-signup');
-          } else {
-            $state.go('app.departed-tab.home');
-          }
-        })
-        .catch(function(err) {
-          $log.error("We got an error! ", err);
-        })
+      if (refGuest) {
+        userService
+          .updateRefGuest(vm.newUser)
+           .then(function(res) {
+                $log.info('Successfully created user, ' + res.data.data.email + ' ' +  res.data.id, res.data);
+                return authService.logIn(vm.newUser);
+            })
+            .then(function(decodedToken) {
+              $log.info('Logged In via Auth service login. ', decodedToken);
+              $state.go('app.departed-tab.home');
+            })
+            .catch(function(err) {
+              $log.error("We got an error! ", err);
+            })
+      } else {
+        userService
+          .signup(vm.newUser)
+          .then(function(res) {
+              $log.info('Successfully created user, ' + res.data.data.email + ' ' +  res.data.id, res.data);
+              return authService.logIn(vm.newUser);
+          })
+          .then(function(decodedToken) {
+            $log.info('Logged In via Auth service login. ', decodedToken);
+            if (vm.vendor) {
+              $state.go('app.vendor-signup');
+            } else {
+              $state.go('app.departed-tab.home');
+            }
+          })
+          .catch(function(err) {
+            $log.error("We got an error! ", err);
+          })
+
+      }
     }
     
     // HELPERS
