b4efc83dd0c9b9579c4b8a325572e3d0cec2390e
myunghoon kim <myungtkim@gmail.com>
2016-08-09 11:36:01 -0700

    adds a matchkey for dynamic traveling

--- www/views/main/schedule/schedule.controller.js
+++ www/views/main/schedule/schedule.controller.js
@@ -6,9 +6,9 @@
     .module('Controllers')
     .controller('Schedule.controller', ScheduleController);
   
-  ScheduleController.$inject = ['$log', 'dataService', 'productService'];
+  ScheduleController.$inject = ['$log', 'dataService', 'productService', 'EventStaticInfo', '$state'];
 
-  function ScheduleController($log, dataService, productService) {
+  function ScheduleController($log, dataService, productService, EventStaticInfo, $state) {
     // INSTANTIATIONS
     $log.instantiate('Schedule', 'controller');
     var vm = this;
@@ -24,31 +24,36 @@
 
     vm.schedule['Funeral Home'] = {
       date: wholeSchedule.funeralhome.date.date,
-      location: wholeSchedule.funeralhome.funeralhome.item ? productService.getProduct(wholeSchedule.funeralhome.funeralhome.item).address : null
+      location: wholeSchedule.funeralhome.funeralhome.item ? productService.getProduct(wholeSchedule.funeralhome.funeralhome.item).address : null,
+      matchKey: 'funeralhome'
     }
     if (wholeSchedule.options.ceremony.item !== null) {
       vm.schedule['Ceremony'] = {
         date: wholeSchedule.options.ceremonydate.date,
-        location: productService.getProduct(wholeSchedule.options.ceremony.item).address
+        location: productService.getProduct(wholeSchedule.options.ceremony.item).address,
+        matchKey: 'options'
       }
     }
 
     if (wholeSchedule.options.reception.item !== null) {
       vm.schedule['Reception'] = {
         date: wholeSchedule.options.receptiondate.date,
-        location: productService.getProduct(wholeSchedule.options.reception.item).address
+        location: productService.getProduct(wholeSchedule.options.reception.item).address,
+        matchKey: 'options'
       }
     }
 
     if (wholeSchedule.options.visitation.date !== null) {
       vm.schedule['Visitation'] = {
-        date: wholeSchedule.options.visitation.date
+        date: wholeSchedule.options.visitation.date,
+        matchKey: 'options'
       }
     }
 
     if (wholeSchedule.options.burial.date !== null) {
       vm.schedule['Burial'] = {
-        date: wholeSchedule.options.burial.date
+        date: wholeSchedule.options.burial.date,
+        matchKey: 'options'
       }
     }
 
@@ -56,6 +61,18 @@
  
 
     // BOUND FUNCTIONS
+    vm.travelToStep = function(key) {
+      var step = EventStaticInfo.filter(function(step) {
+        return step.eventKey === key
+      })[0];
+      dataService.setData(['eventStep', 'stepIndex'], [step, (step.step - 1).toString()])
+      if (key === 'keepsake') {
+        $state.go('app.departed-tab.forms', {tracker: 'program'});
+      } else {
+        $state.go('app.departed-tab.event', {step: step.title})
+
+      }
+    }
 
     // HELPERS
   }
