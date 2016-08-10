6a62f1e4471a4855efd1d13384016e9574a28b20
myunghoon kim <myungtkim@gmail.com>
2016-08-10 14:50:44 -0700

    bugs

--- www/views/main/user_signup/user_signup.controller.js
+++ www/views/main/user_signup/user_signup.controller.js
@@ -32,37 +32,49 @@
           .updateRefGuest(vm.newUser)
            .then(function(res) {
                 // $log.info('Successfully created user, ' + res.data.data.email + ' ' +  res.data.id, res.data);
-                return authService.logIn(vm.newUser);
+              return authService.logIn(vm.newUser);
             })
             .then(function(decodedToken) {
+              return eventService.grabEventPackage(decodedToken._id)
+            })
+            .then(function(events) {
+              // $log.info("event package?!?!?!!?!", events)
+              dataService.removeData(['planningEvents', 'attendingEvents']);
+              dataService.setData(['planningEvents', 'attendingEvents'], [events.planningEvents, events.attendingEvents]);
               // $log.info('Logged In via Auth service login. ', decodedToken);
               $state.go('app.home');
-            })
-            .catch(function(err) {
-              $log.error("We got an error! ", err);
-            })
+           })
+          .catch(function(err) {
+            $log.error("We got an error! ", err);
+          })
       } else {
         userService
           .signup(vm.newUser)
           .then(function(res) {
               // $log.info('Successfully created user, ' + res.data.data.email + ' ' +  res.data.id, res.data);
-              return authService.logIn(vm.newUser);
+            return authService.logIn(vm.newUser);
           })
           .then(function(decodedToken) {
             // $log.info('Logged In via Auth service login. ', decodedToken);
             if (vm.vendor) {
               $state.go('app.vendor-signup');
             } else {
-              $state.go('app.home');
+              eventService
+                .grabEventPackage(decodedToken._id)
+                .then(function(events) {
+                  // $log.info("event package?!?!?!!?!", events)
+                  dataService.removeData(['planningEvents', 'attendingEvents']);
+                  dataService.setData(['planningEvents', 'attendingEvents'], [events.planningEvents, events.attendingEvents]);
+                  $state.go('app.home');
+                })
             }
           })
           .catch(function(err) {
             $log.error("We got an error! ", err);
           })
-
-      }
+        
     }
-    
+  }
     // HELPERS
   }
   
