7e9e6c8e14ac24139b7807128cd2f99263488c1f
myunghoon kim <myungtkim@gmail.com>
2016-08-18 16:05:17 -0700

    data bindings user profile controller

--- www/views/main/user_profile/user_profile.controller.js
+++ www/views/main/user_profile/user_profile.controller.js
@@ -5,8 +5,8 @@
     .module('Controllers')
     .controller('UserProfile.controller', UserProfileController);
 
-  UserProfileController.$inject = ['$log', '$http', 'urlFactory', 'dataService', 'authService', '$state', 'userService' ];
-  function UserProfileController($log, $http, urlFactory, dataService, authService, $state, userService ) {
+  UserProfileController.$inject = ['$log', '$http', 'urlFactory', 'dataService', 'authService', '$state', 'userService', 'uploadService' ];
+  function UserProfileController($log, $http, urlFactory, dataService, authService, $state, userService, uploadService ) {
     var vm = this;
     vm.default = true
     var userId = authService.currentUser()._id
@@ -18,11 +18,19 @@
       } )
 
     vm.changeUser = function() {
-      console.log("Updating the user ?", vm.user)
-      userService.updateUser( vm.user )
-        .then( function( response ) {
-          vm.user = response.data.data
-        })
+      console.log("Updating the user ?", vm.user);
+      if (vm.user.newImage) {
+        uploadService
+          .uploadFile(vm.user.newImage, null, 'user')
+          .then(function(photoUrl) {
+            vm.user.image = photoUrl
+            userService.updateUser( vm.user )
+              .then( function( response ) {
+                vm.user = response.data.data
+              })
+          })
+
+      }
     }
 
     vm.logout = function() {
