9705aaf7b849849828a4530901229828b409b3a5
myunghoon kim <myungtkim@gmail.com>
2016-08-23 16:38:30 -0700

    error handler service to handle future error codes

---
+++ www/core/factories/errorHandler.service.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('Services')
+    .factory('errorHandlerService', errorHandlerService);
+  
+  errorHandlerService.$inject = ['$log'];
+
+  function errorHandlerService($log) {
+    $log.instantiate('Error Handler', 'Service');
+
+    var service = {
+      parseErrorCodes: parseErrorCodes
+    };
+
+    function parseErrorCodes(errCode) = {
+      return switch (errCode) {
+        case 482:
+          return 'Error: Code already submitted by User:: ' + errCode.body.first + " " + errCode.body.last
+          break;
+      }
+    }
+
+
+
+    return service;
+  }
+})();
